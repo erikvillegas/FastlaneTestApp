9687ad5e3cda14ec94718dc28c21d99e599403fa
Erik Villegas <erikv@yelp.com>
2016-09-25 10:16:55 -0700

    added unit tests

---
+++ FastlaneTestAppUnitTests/FastlaneTestAppUnitTests.swift
@@ -0,0 +1,18 @@
+//
+//  FastlaneTestAppUnitTests.swift
+//  FastlaneTestAppUnitTests
+//
+//  Created by Erik Villegas on 9/25/16.
+//  Copyright © 2016 Erik Villegas. All rights reserved.
+//
+
+import XCTest
+
+class FastlaneTestAppUnitTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssert(1 + 1 == 2)
+    }
+}
